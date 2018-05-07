cc6dc78a423e23c0bc559265a0df3f4a2423b17a
Mirko <mirkoskroch@gmail.com>
2018-05-07 19:28:25 +0200

    Initial commit

---
+++ Assets/LineNetworkCreator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace GlowinBallsNetwork
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [RequireComponent(typeof(LineRenderer))]
+    public class LineNetworkCreator : MonoBehaviour 
+    {
+        #region Variable Declarations 
+        // Private Serializable
+
+        // Private
+        LineRenderer lineRenderer;
+        #endregion
+ 
+ 
+ 
+        #region Public Properties
+        
+        #endregion
+ 
+ 
+ 
+        #region Unity Event Functions
+        private void Start()
+        {
+            lineRenderer = GetComponent<LineRenderer>();
+        }
+        #endregion
+ 
+ 
+ 
+        #region Public Functions
+        
+        #endregion
+ 
+ 
+ 
+        #region Private Functions
+        
+        #endregion
+ 
+ 
+ 
+        #region Coroutines
+        
+        #endregion
+    }
+}
